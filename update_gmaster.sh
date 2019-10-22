ca41245f9caa4fd635ebb5a986fa092c31827899
Trevor Pering <peringknife@google.com>
2019-10-22 11:08:52 -0700

    Adding initial gupdater switch

---
+++ update_gmaster.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+
+# Update this to reflect the feature branches that should be merged in.
+PROJ=forch
+BRANCHES=""
+
+TMP_SH=/tmp/update_gmaster.sh
+BASE=`git rev-parse --show-toplevel`
+BASE_SH=$BASE/update_gmaster.sh
+VTEMP=/tmp/GVERSION
+VFILE=$BASE/$PROJ/GVERSION
+date > $VTEMP
+
+if [ $0 != $TMP_SH ]; then
+    echo Running out of $TMP_SH to mask local churn...
+    cp $0 $TMP_SH
+    $TMP_SH; false
+fi
+
+branch=`git rev-parse --abbrev-ref HEAD`
+if [ "$branch" != "gupdater" ]; then
+    echo $0 should be run from the gupdater branch.
+    false
+fi
+
+files=`git status --porcelain`
+if [ -n "$files" ]; then
+    echo Local changes detected:
+    echo $files
+    echo Please resolve before updating.
+    false
+fi
+
+git remote -v | egrep ^origin | fgrep \(fetch >> $VTEMP
+
+echo Fetching remote repos...
+git fetch
+
+LOCAL=`git rev-parse gupdater`
+echo $LOCAL gupdater >> $VTEMP
+REMOTE=`git rev-parse origin/gupdater`
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo gupdater out of sync with upstream origin/gupdater
+    false
+fi
+
+echo Switching to gmaster branch...
+git checkout gmaster
+
+echo Creating clean clone of master...
+git reset --hard origin/master
+echo `git rev-parse HEAD` master >> $VTEMP
+
+echo Merging feature branches...
+for branch in $BRANCHES; do
+    echo Merging origin/$branch...
+    git merge --no-edit origin/$branch
+    echo `git rev-parse origin/$branch` $branch >> $VTEMP
+done
+
+echo `git rev-parse HEAD` gmaster >> $VTEMP
+cp $VTEMP $VFILE
+git add $VFILE
+git commit -m "Adding version history"
+
+echo Done with clean gmaster merge.
+echo You will likely need to force push.
