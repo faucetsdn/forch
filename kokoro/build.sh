9d5709cd843ea6390a0b61120f68152e949cd9f1
Anurag Porripireddi <anuragprdi@google.com>
2019-12-03 15:39:14 -0800

    Kokoro changes, dh_strip to avoid dbgsym, package script
    Change-Id: Iae7ec67b7733902c41236e87d787b2567b87409e

---
+++ kokoro/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Should be run through go/benz
+
+set -eux
+ls -alrt git/
+uname -a
+echo "${TMPDIR}"
+cd git/benz-build-source
+sudo kokoro/setup.sh
+ls -alrt /
+#build-debs -b -L -d rodete
+mkdir -p "${TMPDIR}/binary/"
+mkdir -p "${TMPDIR}/glinux-build"
+debchange --newversion $(git describe --tags $(git rev-list --tags --max-count=1)) -b "New upstream release"
+glinux-build -type="binary" -base-path="${TMPDIR}/glinux-build" -additional-repos="enterprise-sdn-faucet-core-unstable" -name="rodete" . "${TMPDIR}/binary/"
+mkdir -p binary
+cp ${TMPDIR}/binary/* binary/
+#cp ${TMPDIR}/binary/*.deb binary/
+#cp ${TMPDIR}/binary/*.build binary/
+#cp ${TMPDIR}/binary/*.buildinfo binary/
+#cp ${TMPDIR}/binary/*.changes binary/
+
