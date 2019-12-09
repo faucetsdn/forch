6dc03940c8aae05e3c4998a46a08c3aabfc7aca2
Anurag Porripireddi <anuragprdi@google.com>
2019-12-08 19:33:12 -0800

    fix build.sh
    Change-Id: I277b39acdd0b7a1deab0f25a13a844d0c8cbaadc

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/forch
+benz build --git -pool="rodete-huge" -sign -confirm -branch="master" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/forch
--- kokoro/build.sh
+++ kokoro/build.sh
@@ -8,8 +8,6 @@ cd git/benz-build-source
 sudo kokoro/setup.sh
 #mkdir binary/
 #glinux-build -name="rodete" binary/
-bin/setup_base
-bin/build_forch
 debchange --newversion $(git describe --tags $(git rev-list --tags --max-count=1)) -b "New upstream release"
 build-debs -b -L -d rodete
 #cd
