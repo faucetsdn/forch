a447b0d25254b7cce26ab030ee96535f7c4c914b
Anurag Porripireddi <anuragprdi@google.com>
2019-12-17 16:53:05 -0800

    remove build debs
    Change-Id: Ife24630353955b72832e5799c5faf6fe8ac6bcf7

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster" -target-prefix=enterprise-sdn-faucet-forch rpc://perry-internal/forch
+benz build --git -pool="rodete-huge" -sign -confirm -branch="test_build" -target-prefix=enterprise-sdn-faucet-forch rpc://perry-internal/forch
--- kokoro/build.sh
+++ kokoro/build.sh
@@ -9,7 +9,7 @@ sudo kokoro/setup.sh
 #mkdir binary/
 #glinux-build -name="rodete" binary/
 debchange --newversion $(git describe) -b "New upstream release"
-build-debs -b -L -d rodete
+#build-debs -b -L -d rodete
 #cd
 #sudo apt-get install tree
 #tree
