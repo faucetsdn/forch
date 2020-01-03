f4daf7f0414a556af17757d6d72ff9c9581408cf
Trevor Pering <peringknife@google.com>
2020-01-02 22:31:06 -0800

    Try direct remove

--- faucet/valve.py
+++ faucet/valve.py
@@ -130,15 +130,13 @@ class Valve:
         if labels is None:
             labels = self.dp.base_prom_labels()
         metrics_var = getattr(self.metrics, var)
-        print('_set_var', var, labels)
         metrics_var.labels(**labels).set(val)
 
     def _remove_var(self, var, labels=None):
         if labels is None:
             labels = self.dp.base_prom_labels()
         metrics_var = getattr(self.metrics, var)
-        print('_remove_var', var, labels)
-        metrics_var.labels(**labels).remove()
+        metrics_var.remove(**labels)
 
     def close_logs(self):
         """Explicitly close any active loggers."""
