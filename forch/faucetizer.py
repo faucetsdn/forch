e785bea9a251a222a6d90814d6f3a6f4514b58cc
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-01-27 10:49:59 -0800

    changes

--- forch/faucetizer.py
+++ forch/faucetizer.py
@@ -11,32 +11,9 @@ from forch.forchestrator import configure_logging
 from forch.utils import yaml_proto
 
 from forch.proto.network_state_pb2 import NetworkState
+from forch.proto.device_pb2 import Device
 
 LOGGER = logging.getLogger('faucetizer')
-
-
-class Device:
-    """A device with learning and behavior"""
-    def __init__(self):
-        self.old_learning = None
-        self.new_learning = None
-        self.behavior = None
-
-    def is_dirty(self):
-        """Determine if the switch or port of a device has changed"""
-        if not self.old_learning:
-            return False
-        is_same_switch = self.old_learning.switch != self.new_learning.switch
-        is_same_port = self.old_learning.port != self.new_learning.port
-        return not is_same_switch or not is_same_port
-
-    def is_complete(self):
-        """Determine if the info of device is enough to update output faucet config"""
-        return self.new_learning and self.behavior
-
-    def commit(self):
-        """Commit changes of this device"""
-        self.old_learning = copy.deepcopy(self.new_learning)
 
 
 class Faucetizer:
@@ -44,99 +21,57 @@ class Faucetizer:
     def __init__(self, output_file):
         self._devices = {}
         self._base_faucet_config = None
-        self._out_faucet_config = None
         self._output_file = output_file
 
     def process_network_state(self, network_state):
         """Process network state input"""
-        macs = set()
         for mac, learning in network_state.device_mac_learnings.items():
-            macs.add(mac)
             self._process_device_learning(mac, learning)
         for mac, behavior in network_state.device_mac_behaviors.items():
-            macs.add(mac)
             self._process_device_behavior(mac, behavior)
 
-        self._faucetize(macs)
+        self._faucetize()
 
     def _process_device_learning(self, mac, learning):
         device = self._devices.setdefault(mac, Device())
-        device.new_learning = learning
+        device.learning.CopyFrom(learning)
 
     def _process_device_behavior(self, mac, behavior):
         device = self._devices.setdefault(mac, Device())
-        device.behavior = behavior
+        device.behavior.CopyFrom(behavior)
 
     def process_faucet_config(self, faucet_config):
         """Process faucet config when base faucet config changes"""
         self._base_faucet_config = faucet_config
-        self._out_faucet_config = faucet_config
         self._faucetize()
 
-    def _faucetize(self, macs=None):
-        if not self._out_faucet_config:
-            return
+    def _faucetize(self):
+        if not self._base_faucet_config:
+            raise Exception("Base faucet configuration not provided")
 
-        if not macs:
-            macs = self._devices.keys()
-
-        commit = False
-        out_faucet_config = self._out_faucet_config
-
-        for mac in macs:
-            device = self._devices[mac]
-
-            if device.is_dirty():
-                if self._reset_port_config(device, out_faucet_config):
-                    commit = True
-
-            if device.is_complete():
-                switch = device.new_learning.switch
-                port = device.new_learning.port
+        out_faucet_config = copy.deepcopy(self._base_faucet_config)
+        for mac, device in self._devices.items():
+            if device.learning and device.behavior:
+                switch = device.learning.switch
+                port = device.learning.port
                 switch_config = out_faucet_config.get('dps', {}).get(switch, {})
                 port_config = switch_config.get('interfaces', {}).get(port)
+
                 if not port_config:
                     LOGGER.warning('Switch or port not defined in faucet config: %s %s',
                                    switch, port)
                     continue
+
                 port_config['native_vlan'] = device.behavior.vid
                 role = device.behavior.role
                 if role:
                     port_config['acls_in'] = [f'role_{role}']
-                commit = True
 
-        if commit:
-            self._commit_faucet_config(macs, out_faucet_config)
-
-    def _reset_port_config(self, device, out_faucet_config):
-        switch = device.old_learning.switch
-        port = device.old_learning.port
-        switch_config = out_faucet_config.get('dps', {}).get(switch, {})
-        port_config = switch_config.get('interfaces', {}).get(port)
-        if not port_config:
-            LOGGER.warning('Switch or port not defined in faucet config: %s %s', switch, port)
-            return False
-
-        base_switch_config = self._base_faucet_config.get('dps', {}).get(switch, {})
-        base_port_config = base_switch_config.get('interfaces', {}).get(port)
-        if not base_port_config:
-            LOGGER.warning('Switch or port not defined in base faucet config: %s %s', switch, port)
-            return False
-
-        port_config.update(base_port_config)
-
-        return True
-
-    def _commit_faucet_config(self, macs, out_faucet_config):
+    def _generate_faucet_config(self, macs, out_faucet_config):
         try:
             with open(self._output_file, 'w') as config_output:
                 yaml.dump(out_faucet_config, config_output)
 
-            self._out_faucet_config = out_faucet_config
-
-            for mac in macs:
-                device = self._devices[mac]
-                device.commit()
         except IOError as error:
             LOGGER.error('Cannot write faucet config: %s', error)
         except Exception as error:
@@ -184,10 +119,10 @@ if __name__ == '__main__':
     OUTPUT_FILE = os.path.join(FAUCET_BASE_DIR, ARGS.output)
     FAUCETIZER = Faucetizer(OUTPUT_FILE)
 
-    NETWORK_STATE_FILE = os.path.join(FORCH_BASE_DIR, ARGS.state_input)
-    NETWORK_STATE_SAMPLES = load_network_state(NETWORK_STATE_FILE)
-    FAUCETIZER.process_network_state(NETWORK_STATE_SAMPLES)
-
     FAUCET_CONFIG_FILE = os.path.join(FORCH_BASE_DIR, ARGS.config_input)
     FAUCET_CONFIG = load_faucet_config(FAUCET_CONFIG_FILE)
     FAUCETIZER.process_faucet_config(FAUCET_CONFIG)
+
+    NETWORK_STATE_FILE = os.path.join(FORCH_BASE_DIR, ARGS.state_input)
+    NETWORK_STATE_SAMPLES = load_network_state(NETWORK_STATE_FILE)
+    FAUCETIZER.process_network_state(NETWORK_STATE_SAMPLES)
