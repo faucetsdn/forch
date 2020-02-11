8661d2f016dee28bcde174364c25fd1943bb4297
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-02-10 16:35:46 -0800

    fixes

--- forch/faucetizer.py
+++ forch/faucetizer.py
@@ -51,7 +51,7 @@ class Faucetizer:
 
         behavioral_faucet_config = copy.deepcopy(self._structural_faucet_config)
         for mac, device in self._devices.items():
-            if device.placement.switch and device.behavior.vid:
+            if device.placement.switch and device.behavior.segment:
                 switch_cfg = behavioral_faucet_config.get('dps', {}).get(
                     device.placement.switch, {})
                 port_cfg = switch_cfg.get('interfaces', {}).get(device.placement.port)
