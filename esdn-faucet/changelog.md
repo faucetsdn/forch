9f55bc7e08385177e6ed268c6ce036578a4e14c8
Trevor Pering <peringknife@google.com>
2020-03-11 17:58:29 -0400

    Adding esdn-faucet stuff
    Change-Id: I75aaccd0359a2908280f95baba4c012ccd11c858

--- esdn-faucet/changelog.md
+++ esdn-faucet/changelog.md
@@ -1,6 +1,9 @@
+
+# Next Release
+  * ???
 # 1.1.0
-## Forch 0.33.1
-* Faucet 1.9.34.1
+## Forch 0.35
+* Faucet 1.9.34
 * Expose DVA state in NOAH (#45)
 * Ignore dynamic auth result for devices in static file (#43)
 * Add timeouts to session states (#40)
@@ -8,6 +11,8 @@
 * Forch initialization enhancements (#28)
 * Handle L2_EXPIRE events (#27)
 * Update Faucetizer to periodically read and write faucet config (#17)
-## Faucet 1.9.34.1
+## Faucet 1.9.34
 * Add better diagnostics for LACP state
 * Fix Gauge logging spurious "no response" message when prometheus mode is enabled
+
+
---
+++ esdn-faucet/pkg_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+diffs=$(git status -s)
+
+if [ -n "$diffs" ]; then
+  echo Local differences found, please commit/clean before build.
+  git status -s
+  exit 1
+fi
+
+FORCH_VERSION=$(< esdn-faucet/FORCH_VERSION)
+FAUCET_VERSION=$(git show $FORCH_VERSION:etc/FAUCET_VERSION)
+ESDN_VERSION=$(git describe)
+
+echo ESDN_VERSION $ESDN_VERSION
+echo FAUCET_VERSION $FAUCET_VERSION
+echo FORCH_VERSION $FORCH_VERSION
+
+prodaccess
+
+yes | benz build --git -pool="rodete-huge" -sign -branch="$FAUCET_VERSION" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/faucet &
+yes | benz build --git -pool="rodete-huge" -sign -branch="$FORCH_VERSION" -target-prefix=enterprise-sdn-faucet-forch rpc://perry-internal/forch &
+
+wait
+
+echo
+echo Build results:
+
+echo Checkout output of: rapture listrepo enterprise-sdn.faucet.all-unstable
+rapture listrepo enterprise-sdn.faucet.all-unstable | fgrep "esdn $ESDN_VERSION " || (echo Missing ESDN version $ESDN_VERSION && false)
+rapture listrepo enterprise-sdn.faucet.all-unstable | fgrep "faucet $FAUCET_VERSION " || (echo Missing FAUCET verion $FAUCET_VERSION && false)
+rapture listrepo enterprise-sdn.faucet.all-unstable | fgrep "forch $FORCH_VERSION " || (echo Missing FORCH version $FORCH_VERSION && false)
+
+echo
+echo Done with successful build.
