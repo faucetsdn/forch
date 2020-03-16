9fd8ea8a618e17d77e5c48794cb274b64131893f
Anurag Porripireddi <anuragprdi@google.com>
2020-03-16 23:28:03 +0000

    Lint fixes

--- forch/forch_metrics.py
+++ forch/forch_metrics.py
@@ -1,7 +1,11 @@
-import forch.http_server
+"""Module to expose varz interface"""
+
 import functools
 
+import forch.http_server
+
 class ForchMetrics():
+    """Class that implements the module that exposes varz for metrics"""
     def __init__(self, local_port, config):
         self._local_port = local_port
         self._config = config
@@ -9,18 +13,23 @@ class ForchMetrics():
         self._metrics = None
 
     def start(self):
+        """Start serving varz"""
         self._http_server = forch.http_server.HttpServer(self._config, self._local_port)
         try:
-            self._http_server.map_request('', self.get_metrics)
+            self._http_server.map_request('', self._get_metrics)
         except Exception as e:
-            self._http_server.map_request('', functools.partial(show_error, e))
+            self._http_server.map_request('', functools.partial(self._show_error, e))
         finally:
             self._http_server.start_server()
 
     def stop(self):
+        """Kill server"""
         self._http_server.stop_server()
 
-    def get_metrics(self, path, params):
+    def _get_metrics(self, path, params):
         #return self._metrics
         return "Version 1"
 
+    def _show_error(self, error, path, params):
+        """Display errors"""
+        return f"Error creating varz interface: {str(error)}"
