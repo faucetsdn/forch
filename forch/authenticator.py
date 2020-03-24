58131ea33489f7cc423dca4cc1f96575d2e4bed5
Anurag Porripireddi <anuragprdi@google.com>
2020-03-24 02:52:58 +0000

    Remove auth.yaml dependency

--- forch/authenticator.py
+++ forch/authenticator.py
@@ -19,14 +19,12 @@ from forch.proto.authentication_pb2 import AuthResult
 from forch.proto.forch_configuration_pb2 import OrchestrationConfig
 
 LOGGER = logging.getLogger('auth')
-AUTH_FILE_NAME = 'auth.yaml'
 
 HEARTBEAT_INTERVAL_SEC = 3
 
 class Authenticator:
     """Authenticate devices using MAB/dot1x"""
     def __init__(self, auth_config, auth_callback=None, radius_query_object=None):
-        self.auth_map = self._get_auth_map()
         self.radius_query = None
         self.sessions = {}
         self.auth_callback = auth_callback
@@ -59,27 +57,6 @@ class Authenticator:
         self.timer.start()
         LOGGER.info('Created Authenticator module with radius IP %s and port %s.',
                     radius_ip, radius_port)
-
-    def _get_auth_map(self):
-        base_dir = os.getenv('FORCH_CONFIG_DIR')
-        auth_file = os.path.join(base_dir, AUTH_FILE_NAME)
-        auth_map = None
-        with open(auth_file, 'r') as stream:
-            try:
-                auth_map = yaml.safe_load(stream).get('auth_map')
-            except yaml.YAMLError as exc:
-                LOGGER.error("Error loading yaml file: %s", exc, exc_info=True)
-        return auth_map
-
-    def authenticate(self, device_id):
-        """Returns role and segment for given device_id"""
-        auth_result = {}
-        if device_id in self.auth_map:
-            auth_result = self.auth_map.get(device_id)
-        else:
-            auth_result = self.auth_map.get('default')
-        auth_result['device_id'] = device_id
-        return dict_proto(auth_result, AuthResult)
 
     def process_auth_result(self):
         """Prints Auth example object to out"""
