71ed855fb838f30d4bce67c8e24a6f85e85dda17
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-06-16 23:14:35 -0700

    fix tests

--- testing/unit/test_faucetizer.py
+++ testing/unit/test_faucetizer.py
@@ -301,6 +301,8 @@ class FaucetizerBehaviorTestCase(FaucetizerTestBase):
             expected_config, switch='t2sw1', port=2, vlan=300, role='green', tail_acl='tail_acl')
         self._update_port_config(
             expected_config, switch='t2sw2', port=1, vlan=400, tail_acl='tail_acl')
+        self._update_port_config(
+            expected_config, switch='t2sw2', port=2, vlan=100, tail_acl='tail_acl')
         self._verify_behavioral_config(expected_config)
 
         # device expired
@@ -312,6 +314,10 @@ class FaucetizerBehaviorTestCase(FaucetizerTestBase):
             expected_config, switch='t2sw1', port=1, vlan=200, role='red', tail_acl='tail_acl')
         self._update_port_config(
             expected_config, switch='t2sw1', port=2, vlan=300, role='green', tail_acl='tail_acl')
+        self._update_port_config(
+            expected_config, switch='t2sw2', port=1, vlan=100, tail_acl='tail_acl')
+        self._update_port_config(
+            expected_config, switch='t2sw2', port=2, vlan=100, tail_acl='tail_acl')
         self._verify_behavioral_config(expected_config)
 
 
