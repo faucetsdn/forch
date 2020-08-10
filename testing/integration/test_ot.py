5687cc455082b25c4702f9f687bcc66ca8e3f66f
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-08-09 22:20:39 -0700

    tests changes

--- testing/integration/test_ot.py
+++ testing/integration/test_ot.py
@@ -6,7 +6,7 @@ from forch.faucetizer import Faucetizer
 from forch.utils import dict_proto
 
 from forch.proto.devices_state_pb2 import DevicePlacement, DeviceBehavior
-from forch.proto.forch_configuration_pb2 import ForchConfig
+from forch.proto.forch_configuration_pb2 import OrchestrationConfig
 
 from testing.test_lib.integration_base import IntegrationTestBase, logger
 
@@ -40,23 +40,22 @@ class OTConfigTest(IntegrationTestBase):
         self._clean_stack()
 
     def _generate_sequestering_config(self):
-        forch_config_map = {
-            'orchestration': {
-                'fot_config': {
-                    'testing_segment': 'TESTING',
-                    'testing_vlan_start': 1000,
-                    'testing_vlan_end': 1999,
-                    'testing_port_identifier': 'TESTING'
-                }
+        orch_config_map = {
+            'fot_config': {
+                'testing_segment': 'TESTING',
+                'testing_vlan_start': 1000,
+                'testing_vlan_end': 1999,
+                'testing_port_identifier': 'TESTING'
             }
         }
-        forch_config = dict_proto(forch_config_map, ForchConfig)
+        orch_config = dict_proto(orch_config_map, OrchestrationConfig)
         structural_config_file = self._get_faucet_config_path()
         behivoral_config_file = self._get_faucet_config_path()
         segments_to_vlans = {'HOST': 272}
 
         faucetizer = Faucetizer(
-            forch_config, structural_config_file, segments_to_vlans, behivoral_config_file)
+            orch_config, structural_config_file, segments_to_vlans, behivoral_config_file)
+        faucetizer.reload_structural_config()
 
         mac = '0A:00:00:00:00:01'
         device_placement_map = {
@@ -72,10 +71,6 @@ class OTConfigTest(IntegrationTestBase):
 
         faucetizer.process_device_placement(mac, device_placement)
         faucetizer.process_device_behavior(mac, device_behavior)
-
-        # TODO remove
-        config = self._read_faucet_config()
-        print(config)
 
 
 if __name__ == '__main__':
