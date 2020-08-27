7fadb68ad8e2744522b8b414630e40843eb9d51e
Anurag Porripireddi <anuragprdi@google.com>
2020-08-27 17:51:50 +0000

    Forch inside faucet container

---
+++ bin/install-forch.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+set -euo pipefail
+
+APK="apk -q"
+BUILDDEPS="gcc g++ python3-dev musl-dev parallel yaml-dev"
+TESTDEPS="bitstring pytest wheel virtualenv"
+PIP3="pip3 -q --no-cache-dir install --upgrade"
+FROOT="/forch-src"
+
+dir=$(dirname "$0")
+
+${APK} add -U git ${BUILDDEPS}
+${PIP3} pip
+${PIP3} setuptools ${TESTDEPS}
+${PIP3} -r ${FROOT}/etc/requirements.txt
+${PIP3} ${FROOT}
+
+pip3 uninstall -y ${TESTDEPS} || exit 1
+for i in ${BUILDDEPS} ; do
+    ${APK} del "$i" || exit 1
+done
+
+# Clean up
+rm -r "${FROOT}"
