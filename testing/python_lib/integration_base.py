ab9ab68e210f7d09846658e15a4f9345eab1b8bf
Anurag Porripireddi <anuragprdi@google.com>
2020-10-13 01:13:46 +0000

    Change Pylint pos

--- testing/python_lib/integration_base.py
+++ testing/python_lib/integration_base.py
@@ -41,8 +41,9 @@ class IntegrationTestBase(unittest.TestCase):
         strerr = str(stderr, 'utf-8') if stderr else None
         return process.returncode, strout, strerr
 
+    #pylint: disable=too-many-arguments
     def _run_cmd(self, cmd, arglist=None, strict=True,
-                 capture=False, docker_container=None):  # pylint: disable=too-many-arguments
+                 capture=False, docker_container=None):
         command = ("docker exec %s " % docker_container) if docker_container else ""
         command = command.split() + ([cmd] + arglist) if arglist else command + cmd
         retcode, out, err = self._reap_process_command(
