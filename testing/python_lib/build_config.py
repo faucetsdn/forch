4a463fbb1c8b574d994e68a3dcb6f8e62ef0496c
Anurag Porripireddi <anuragprdi@google.com>
2020-10-27 00:22:14 +0000

    Move config generation to separate class

---
+++ testing/python_lib/build_config.py
@@ -0,0 +1,77 @@
+import sys
+import yaml
+from forch.utils import proto_dict
+from forch.proto.faucet_configuration_pb2 import Interface, StackLink, Datapath, \
+    Vlan, FaucetConfig, LLDPBeacon, Stack
+
+
+class FaucetConfigGenerator():
+    """Class for generating faucet config for given switches and devices per switch"""
+
+    def _build_t1_interfaces(self, dp_index, t1_dps, t2_dps, t2_port, tagged_vlans, tap_vlan=None):
+        interfaces = {}
+        if tap_vlan:
+            interfaces[4] = Interface(description='tap', tagged_vlans=[tap_vlan])
+        for index, dp in enumerate(t1_dps):
+            if abs(dp_index - index) == 1:
+                port = 6 + min(dp_index, index)
+                description = ("to %s port %s" % (dp, port))
+                interfaces[port] = Interface(description=description,
+                                             stack=StackLink(dp=dp, port=port))
+        for index, dp in enumerate(t2_dps):
+            port = 100 + index
+            description = ("to %s port %s" % (dp, t2_port))
+            interfaces[port] = Interface(description=description,
+                                         stack=StackLink(dp=dp, port=t2_port))
+        interfaces[28] = Interface(description='egress', lacp=3, tagged_vlans=tagged_vlans)
+        return interfaces
+
+    def _build_t2_interfaces(self, dp_index, t1_dps, access_ports, native_vlan, port_acl):
+        interfaces = {}
+        for index in range(access_ports):
+            interfaces[index + 101] = Interface(description='IoT Device',
+                                            native_vlan=native_vlan, acl_in=port_acl, max_hosts=1)
+        for index, dp in enumerate(t1_dps):
+            port = 50 + index * 2
+            description = ('to %s port %s' % (dp, 100+dp_index))
+            interfaces[port] = Interface(description=description,
+                                         stack=StackLink(dp=dp, port=100+dp_index))
+        return interfaces
+
+    def _build_datapath_config(self, dp_id, mac, interfaces):
+        lldp_beacon = LLDPBeacon(max_per_interval=5, send_interval=5)
+        stack = Stack(priority=1)
+        return Datapath(dp_id=dp_id, faucet_dp_mac=mac, hardware='Generic', \
+                        lacp_timeout=5, lldp_beacon=lldp_beacon, interfaces=interfaces, stack=stack)
+
+    def _create_scale_faucet_config(self, t1_switches, t2_switches, access_ports):
+        setup_vlan = 171
+        test_vlan = 272
+        vlans = {
+            setup_vlan: Vlan(description='Faucet IoT'),
+            test_vlan: Vlan(description='Orchestrated Testing')
+        }
+        t1_dps = [('nz-kiwi-t1sw%s' % (dp_index + 1)) for dp_index in range(t1_switches)]
+        t2_dps = [('nz-kiwi-t2sw%s' % (dp_index + 1)) for dp_index in range(t2_switches)]
+        dps = {}
+        for dp_index, dp_name in enumerate(t1_dps):
+            tap_vlan = test_vlan if not dp_index else None
+            interfaces = self._build_t1_interfaces(dp_index, t1_dps, t2_dps,
+                                                   50 + dp_index * 2, [setup_vlan], tap_vlan)
+            dps[dp_name] = self._build_datapath_config(
+                177 + dp_index, ('0e:00:00:00:01:%s' % ("{:02x}".format(dp_index+1))), interfaces)
+
+        for dp_index, dp_name in enumerate(t2_dps):
+            interfaces = self._build_t2_interfaces(dp_index,
+                                                t1_dps, access_ports, setup_vlan, 'uniform_acl')
+            dps[dp_name] = self._build_datapath_config(
+                1295 + dp_index, ('0e:00:00:00:02:%s' % ("{:02x}".format(dp_index+1))), interfaces)
+        return FaucetConfig(dps=dps, version=2, include=['uniform.yaml'], vlans=vlans)
+
+
+if __name__ == '__main__':
+    config_generator = FaucetConfigGenerator()
+    filepath = sys.argv[1] if len(sys.argv) > 1 else '/tmp/faucet_config_dump'
+    config = proto_dict(config_generator._create_scale_faucet_config(2, 5, 9))
+    with open(filepath, 'w') as config_file:
+        yaml.dump(config, config_file)
--- testing/python_lib/test_forchestrator.py
+++ testing/python_lib/test_forchestrator.py
@@ -6,10 +6,9 @@ import yaml
 from unit_base import ForchestratorTestBase
 from forch.authenticator import Authenticator
 from forch.port_state_manager import PortStateManager
-from forch.utils import dict_proto, proto_dict
+from forch.utils import dict_proto
 from forch.proto.devices_state_pb2 import DevicePlacement, DeviceBehavior
 from forch.proto.forch_configuration_pb2 import OrchestrationConfig
-from forch.proto.faucet_configuration_pb2 import Interface, StackLink, Datapath, Vlan, FaucetConfig, LLDPBeacon, Stack
 
 
 # pylint: disable=protected-access
@@ -52,66 +51,7 @@ class ForchestratorUnitTestCase(ForchestratorTestBase):
                 tagged_vlans: [171]
             lldp_beacon: {max_per_interval: 5, send_interval: 5}"""
         faucet_config = yaml.safe_load(faucet_config_str)
-        # self.assertFalse(self._forchestrator._validate_config(faucet_config))
-        config = proto_dict(self._create_scale_faucet_config(2, 3, 10))
-        print(config)
-        with open('/tmp/yaml_dump.yaml', 'w') as config_file:
-            yaml.dump(config, config_file)
-        return
-
-    def _build_t1_interfaces(self, dp_index, t1_dps, t2_dps, t2_port, tagged_vlans, tap_vlan=None):
-        interfaces = {}
-        if tap_vlan:
-            interfaces[4] = Interface(description='tap', tagged_vlans=[tap_vlan])
-        for index, dp in enumerate(t1_dps):
-            if abs(dp_index - index) == 1:
-                port = 6 + min(dp_index, index)
-                description = ("to %s port %s" % (dp, port))
-                interfaces[port] = Interface(description=description, stack=StackLink(dp=dp, port=port))
-        for index, dp in enumerate(t2_dps):
-            port = 100 + index
-            description = ("to %s port %s" % (dp, t2_port))
-            interfaces[port] = Interface(description=description, stack=StackLink(dp=dp, port=t2_port))
-        interfaces[28] = Interface(description='egress', lacp=3, tagged_vlans=tagged_vlans)
-        return interfaces
-
-    def _build_t2_interfaces(self, dp_index, t1_dps, access_ports, native_vlan, port_acl):
-        interfaces = {}
-        for index in range(access_ports):
-            interfaces[index + 101] = Interface(description='IoT Device', native_vlan=native_vlan, acl_in=port_acl, max_hosts=1)
-        for index, dp in enumerate(t1_dps):
-            port = 50 + index * 2
-            description = ('to %s port %s' % (dp, 100+dp_index))
-            interfaces[port] = Interface(description=description, stack=StackLink(dp=dp, port=100+dp_index))
-        return interfaces
-
-    def _build_datapath_config(self, dp_id, mac, interfaces):
-        lldp_beacon = LLDPBeacon(max_per_interval=5, send_interval=5)
-        stack = Stack(priority=1)
-        return Datapath(dp_id=dp_id, faucet_dp_mac=mac, hardware='Generic', lacp_timeout=5, lldp_beacon=lldp_beacon, interfaces=interfaces, stack=stack)
-
-    def _create_scale_faucet_config(self, t1_switches, t2_switches, access_ports):
-        setup_vlan = 171
-        test_vlan = 272
-        vlans = {
-            str(setup_vlan): Vlan(description='Faucet IoT'),
-            str(test_vlan): Vlan(description='Orchestrated Testing')
-        }
-        t1_dps = [('nz-kiwi-t1sw%s' % (dp_index + 1)) for dp_index in range(t1_switches)]
-        t2_dps = [('nz-kiwi-t2sw%s' % (dp_index + 1)) for dp_index in range(t2_switches)]
-        dps = {}
-        for dp_index, dp_name in enumerate(t1_dps):
-            tap_vlan = test_vlan if not dp_index else None
-            interfaces = self._build_t1_interfaces(dp_index, t1_dps, t2_dps, 50 + dp_index * 2, [setup_vlan], tap_vlan)
-            print(interfaces)
-            dps[dp_name] = self._build_datapath_config(177 + dp_index, ('0e:00:00:00:01:%s' % ("{:02x}".format(dp_index+1))), interfaces)
-
-        for dp_index, dp_name in enumerate(t2_dps):
-            interfaces = self._build_t2_interfaces(dp_index, t1_dps, access_ports, setup_vlan, 'uniform_acl')
-            print(interfaces)
-            dps[dp_name] = self._build_datapath_config(1295 + dp_index, ('0e:00:00:00:02:%s' % ("{:02x}".format(dp_index+1))), interfaces)
-
-        return FaucetConfig(dps=dps, version=2, include=['uniform.yaml'], vlans=vlans)
+        self.assertFalse(self._forchestrator._validate_config(faucet_config))
 
 
 # pylint: disable=protected-access
