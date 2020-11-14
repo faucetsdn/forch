ad74da747ffdbdf4da500b00a505bd22283152d5
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-11-13 18:48:44 -0800

    update unit test

--- testing/python_lib/test_fot.py
+++ testing/python_lib/test_fot.py
@@ -296,7 +296,7 @@ class FotContainerTest(IntegrationTestBase):
         """Test to check DHCP reflection when on test VLAN"""
         # trigger learning event for faux-1 to make it authenticated and sequestered
         self._run_cmd('ping -c1 8.8.8.8', docker_container='forch-faux-1', strict=False)
-        sleep 5
+        time.sleep(5)
 
         # test DHCP reflection with sequestered device
         device_tcpdump_text, vlan_tcpdump_text = self._internal_dhcp('forch-faux-1')
