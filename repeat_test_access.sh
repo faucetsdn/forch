2aed6ac28a17d2f7e78e5fc01b78d52c3732a650
Anurag Porripireddi <anuragprdi@google.com>
2021-01-26 06:41:25 +0000

    Revert unnecessary files.

--- repeat_test_access.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-role='"yellow"'
-echo $role
-while [ $role == '"yellow"' ]
-do
-    bin/net_clean; bin/setup_stack local skip-conn-check dva; bin/run_test_set access
-    role=$(jq '.eth_srcs."9a:02:57:1e:8f:03".radius_result.role' test_out/dva-mac-flipped/list_hosts.json)
-done
