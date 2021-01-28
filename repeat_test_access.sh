b38ac8faf7fc61b1b40a626b5d638373ecb2457d
Anurag Porripireddi <anuragprdi@google.com>
2021-01-28 20:00:30 +0000

    Remove repeat_test_access

--- repeat_test_access.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-role=0
-echo $role
-while [ $role == 0 ]
-do
-    bin/net_clean; bin/setup_stack local skip-conn-check dva; bin/run_test_set access
-    role=$(jq '.switches."nz-kiwi-t2sw3".ports."1".acls[0].rules[0].packet_count' test_out/dva-port-down/switch_state.json)
-done
