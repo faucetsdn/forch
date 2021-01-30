23a93f3c2f2932c25a9021c4e0fb449186a773ac
Trevor Pering <peringknife@google.com>
2021-01-29 20:55:53 -0800

    Using vlan 500

--- testing/python_lib/build_config.py
+++ testing/python_lib/build_config.py
@@ -187,9 +187,8 @@ class FaucetConfigGenerator():
 
         return FaucetConfig(dps=dps, version=2, include=['uniform.yaml'], vlans=vlans)
 
-    def create_corp_faucet_config(self):
+    def create_corp_faucet_config(self, setup_vlan):
         """Create Faucet config for corp network"""
-        setup_vlan = SETUP_VLAN
         switch = 'corp'
         dps = {}
 
@@ -210,6 +209,7 @@ def main(argv):
     devices = 1
     topo_type = STACK
     argv = argv[1:]
+    vlan_id = SETUP_VLAN
 
     help_msg = """
     <python3> build_config.py -e <egress_switches> -a <access_switches> -d <devices per switch>
@@ -218,7 +218,7 @@ def main(argv):
 
     try:
         opts, _ = getopt.getopt(
-            argv, 'he:a:d:p:t:', ['egress=', 'access=', 'devices=', 'path=', 'type='])
+            argv, 'he:a:d:p:t:v:', ['egress=', 'access=', 'devices=', 'path=', 'type=', 'vlan='])
     except getopt.GetoptError:
         print(help_msg)
         sys.exit(2)
@@ -236,11 +236,13 @@ def main(argv):
             filepath = arg
         elif opt in ('-t', '--type'):
             topo_type = arg
+        elif opt in ('-v', '--vlan'):
+            vlan_id = int(arg)
 
     if topo_type == FLAT:
         faucet_config = config_generator.create_flat_faucet_config(access, devices)
     elif topo_type == CORP:
-        faucet_config = config_generator.create_corp_faucet_config()
+        faucet_config = config_generator.create_corp_faucet_config(vlan_id)
     elif topo_type == STACK:
         faucet_config = config_generator.create_scale_faucet_config(egress, access, devices)
     else:
