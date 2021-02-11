4f504833b4b6577fdabfc3d0e61ea965b54bbad3
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2021-02-11 11:03:10 -0800

    build proto

--- forch/proto/vlan_state_pb2.py
+++ forch/proto/vlan_state_pb2.py
@@ -19,29 +19,29 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1c\x66orch/proto/vlan_state.proto\"2\n\tVLANState\x12\x0f\n\x07vlan_id\x18\x01 \x01(\x05\x12\x14\n\x0cpacket_count\x18\x02 \x01(\x05\x62\x06proto3'
+  serialized_pb=b'\n\x1c\x66orch/proto/vlan_state.proto\"2\n\tVlanState\x12\x0f\n\x07vlan_id\x18\x01 \x01(\x05\x12\x14\n\x0cpacket_count\x18\x02 \x01(\x05\x62\x06proto3'
 )
 
 
 
 
 _VLANSTATE = _descriptor.Descriptor(
-  name='VLANState',
-  full_name='VLANState',
+  name='VlanState',
+  full_name='VlanState',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='vlan_id', full_name='VLANState.vlan_id', index=0,
+      name='vlan_id', full_name='VlanState.vlan_id', index=0,
       number=1, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='packet_count', full_name='VLANState.packet_count', index=1,
+      name='packet_count', full_name='VlanState.packet_count', index=1,
       number=2, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -63,15 +63,15 @@ _VLANSTATE = _descriptor.Descriptor(
   serialized_end=82,
 )
 
-DESCRIPTOR.message_types_by_name['VLANState'] = _VLANSTATE
+DESCRIPTOR.message_types_by_name['VlanState'] = _VLANSTATE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-VLANState = _reflection.GeneratedProtocolMessageType('VLANState', (_message.Message,), {
+VlanState = _reflection.GeneratedProtocolMessageType('VlanState', (_message.Message,), {
   'DESCRIPTOR' : _VLANSTATE,
   '__module__' : 'forch.proto.vlan_state_pb2'
-  # @@protoc_insertion_point(class_scope:VLANState)
+  # @@protoc_insertion_point(class_scope:VlanState)
   })
-_sym_db.RegisterMessage(VLANState)
+_sym_db.RegisterMessage(VlanState)
 
 
 # @@protoc_insertion_point(module_scope)
