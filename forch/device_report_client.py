e11a86a3029c8ec03d55041872537a64e59ea9e5
Trevor Pering <peringknife@google.com>
2021-04-12 08:32:39 -0700

    Clean commit

--- forch/device_report_client.py
+++ forch/device_report_client.py
@@ -1,13 +1,18 @@
-"""gRPC client to send device result"""
+"""Server to handle incoming session requests"""
 
 import grpc
 
-try:
-    from daq.proto.session_server_pb2_grpc import SessionServerStub
-except ImportError:
-    pass
-
 from forch.base_classes import DeviceStateReporter
+from forch.utils import get_logger
+
+LOGGER = get_logger('devreport')
+
+try:
+    from daq.proto.session_server_pb2 import SessionParams
+    from daq.proto.session_server_pb2_grpc import SessionServerStub
+    LOGGER.info('imported daq dependencies')
+except ImportError as e:
+    LOGGER.error('daq dependencies not found: %s', e)
 
 
 DEFAULT_SERVER_ADDRESS = '127.0.0.1'
@@ -17,18 +22,24 @@ DEFAULT_RPC_TIMEOUT_SEC = 10
 
 class DeviceReportClient(DeviceStateReporter):
     """gRPC client to send device result"""
-    def __init__(self, result_handler, server_address, server_port):
-        self._initialize_stub(server_address or DEFAULT_SERVER_ADDRESS,
-                              server_port or DEFAULT_SERVER_PORT)
 
+    def __init__(self, result_handler, server_address, server_port):
+        address = server_address or DEFAULT_SERVER_ADDRESS
+        port = server_port or DEFAULT_SERVER_PORT
+        channel = grpc.insecure_channel(f'{address}:{port}')
+        self._stub = SessionServerStub(channel)
         self._rpc_timeout_sec = DEFAULT_RPC_TIMEOUT_SEC
 
-    def _initialize_stub(self, sever_address, server_port):
-        channel = grpc.insecure_channel(f'{sever_address}:{server_port}')
-        self._stub = SessionServerStub(channel)
-
     def start(self):
-        """Start a client connection"""
+        """Initiate a client connection"""
+        LOGGER.info('Starting DeviceReportClient')
+        session_params = SessionParams()
+        session_params.device_mac = 'devicemac'
+        self._stub.StartSession(session_params,
+                                timeout=self._rpc_timeout_sec)
+
+    def stop(self):
+        """Terminates all onging grpc calls"""
 
     def process_port_state(self, dp_name, port, state):
         """Process faucet port state events"""
@@ -38,6 +49,3 @@ class DeviceReportClient(DeviceStateReporter):
 
     def process_port_assign(self, mac, assigned):
         """Process faucet port vlan assignment"""
-
-    def stop(self):
-        """Terminates all onging grpc calls"""
