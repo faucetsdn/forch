ae604163614b43ea85cb4c3a1d354e9f05b512e0
Trevor Pering <peringknife@google.com>
2021-05-26 10:40:56 -0700

    Cleanup kokoro/check.sh

--- kokoro/check.sh
+++ kokoro/check.sh
@@ -16,35 +16,36 @@ done
 
 # Check if faucet tags were added into forch, in remote or local
 bad_r=""
+faucet_bad=
 for remote in faucet origin perry; do
     faucet_tag=`git ls-remote -t $remote | grep v1_0` || true
     if [ -n "$faucet_tag" ]; then
         bad_r=${bad_r}" $remote"
+        faucet_bad=$faucet_tag" $faucet_tag"
     fi
 done
 faucet_tag=`git tag --list | grep v1_0` || true
 if [ -n "$faucet_tag" ]; then
     bad_r=${bad_r}" local"
+    faucet_bad=$faucet_tag" $faucet_tag"
 fi
 if [ -n "$bad_r" ]; then
     echo
     echo Error:
     echo Faucet tag found in Forch repos in: [ $bad_r ]
-    echo
+    echo $faucet_bad > .bad_tags
+    echo All bad tags dumped to file .bad_tags
     false
 fi
 
 mtag=`git describe perry/master --abbrev=0`
 gtag=`git describe perry/gmaster --abbrev=0`
-etag=`git describe perry/esdn --abbrev=0`
 
 mhash=`git rev-list -n 1 $mtag`
 ghash=`git rev-list -n 1 $gtag`
-ehash=`git rev-list -n 1 $etag`
 
 mbase=`git merge-base $mtag perry/master`
 gbase=`git merge-base $gtag perry/gmaster`
-ebase=`git merge-base $etag perry/esdn`
 
 if [ $mbase != $mhash ]; then
     echo Merge base for master/$mtag does not match expected hash.
@@ -53,11 +54,6 @@ fi
 
 if [ $gbase != $ghash ]; then
     echo Merge base for gmaster/$gtag does not match expected hash.
-    false
-fi
-
-if [ $ebase != $ehash ]; then
-    echo Merge base for esdn/$etag does not match expected hash.
     false
 fi
 
@@ -96,33 +92,6 @@ fi
 if [ -z "$og" ]; then
     echo Unknown tag $gtag
     false
-fi
-
-echo Checking remote esdn tag $etag
-oe=`git ls-remote origin $etag`
-pe=`git ls-remote perry $etag`
-if [ "$oe" != "$pe" ]; then
-    echo origin esdn: $oe
-    echo pperry esdn: $pe
-    false
-fi
-
-if [ -z "$oe" ]; then
-    echo Unknown tag $etag
-    false
-fi
-
-mbase=`git merge-base $gtag $etag`
-mref=`git rev-list -n 1 $gtag`
-if [ "$mbase" != "$mref" ]; then
-    echo
-    echo Error:
-    echo "  git merge-base $gtag $etag"
-    echo does not match expected gmaster tag $gtag
-    echo
-    false
-else
-    echo Merge-base of $gtag and perry/master is $mtag
 fi
 
 echo
